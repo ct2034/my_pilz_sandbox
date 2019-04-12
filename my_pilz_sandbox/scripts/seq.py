627759a2e0a47ac6b08d7f21c259e429f78cf21f
Christian Henkel <post@henkelchristian.de>
2019-04-12 13:53:51 +0200

    fun with sequences

--- my_pilz_sandbox/scripts/seq.py
+++ my_pilz_sandbox/scripts/seq.py
@@ -16,47 +16,85 @@ GRIPPER_POSE_OPEN = 0.029
 
 # trying circ command
 def sequence(r):
-    seq = Sequence()
-    seq.append(Gripper(goal=GRIPPER_POSE_CLOSED))
-
-    seq.append(Lin(goal=Pose(position=Point(0.2, 0, 1)),
+    r.move(Ptp(goal=Pose(position=Point(0.0, 0.0, 1.1), orientation=Quaternion(0,0,0,1)),
                     vel_scale=SLOW_VEL_SCALE,
                     acc_scale=ACC_SCALE))
-    seq.append(Lin(goal=Pose(position=Point(0.2, 0.2, 1)),
+    r.move(Ptp(goal=Pose(position=Point(0.0, 0.0, .9), orientation=Quaternion(0,0,0,1)),
+                    vel_scale=SLOW_VEL_SCALE,
+                    acc_scale=ACC_SCALE))
+    print("prepared.")
+
+    seq = Sequence()
+
+    seq.append(Ptp(goal=Pose(position=Point(0.0, 0, .9), orientation=Quaternion(0,0,0,1)),
+                    vel_scale=SLOW_VEL_SCALE,
+                    acc_scale=ACC_SCALE))
+    seq.append(Ptp(goal=Pose(position=Point(0.2, 0, .9), orientation=Quaternion(0,0,0,1)),
+                    vel_scale=SLOW_VEL_SCALE,
+                    acc_scale=ACC_SCALE),
+		blend_radius=0.099)
+    seq.append(Ptp(goal=Pose(position=Point(0.2, 0.2, .9), orientation=Quaternion(0,0,0,1)),
                    vel_scale=SLOW_VEL_SCALE,
                    acc_scale=ACC_SCALE),
-               blend_radius=0.19)
-    seq.append(Lin(goal=Pose(position=Point(0, 0.2, 1)),
+               blend_radius=0.099)
+    seq.append(Ptp(goal=Pose(position=Point(0, 0.2, .9), orientation=Quaternion(0,0,0,1)),
                    vel_scale=SLOW_VEL_SCALE,
                    acc_scale=ACC_SCALE))
-
-    seq.append(Gripper(goal=GRIPPER_POSE_OPEN))
-    seq.append(Gripper(goal=GRIPPER_POSE_CLOSED))
 
     print("start sequence 1")
     r.move(seq)
     print("end sequence 1")
 
-    seq = Sequence()
-    seq.append(Gripper(goal=GRIPPER_POSE_CLOSED))
+    r.move(Ptp(goal=Pose(position=Point(0.0, 0.0, .9), orientation=Quaternion(0,0,0,1)),
+                    vel_scale=SLOW_VEL_SCALE,
+                    acc_scale=ACC_SCALE))
+    print("prepared.")
 
-    seq.append(Lin(goal=Pose(position=Point(0, 0.2, 1)),
-        vel_scale=SLOW_VEL_SCALE,
-        acc_scale=ACC_SCALE))
-    seq.append(Lin(goal=Pose(position=Point(0.2, 0.2, 1)),
+    seq = Sequence()
+
+    seq.append(Ptp(goal=Pose(position=Point(0.0, 0, .9), orientation=Quaternion(0,0,0,1)),
+                    vel_scale=SLOW_VEL_SCALE,
+                    acc_scale=ACC_SCALE))
+    seq.append(Ptp(goal=Pose(position=Point(0.2, 0, .9), orientation=Quaternion(0,0,0,1)),
+                    vel_scale=SLOW_VEL_SCALE,
+                    acc_scale=ACC_SCALE),
+		blend_radius=0.099)
+    seq.append(Ptp(goal=Pose(position=Point(0.2, 0.2, .9), orientation=Quaternion(0,0,0,1)),
                    vel_scale=SLOW_VEL_SCALE,
                    acc_scale=ACC_SCALE),
-               blend_radius=0.19)
-    seq.append(Gripper(goal=GRIPPER_POSE_OPEN))
-    seq.append(Lin(goal=Pose(position=Point(0.2, 0, 1)),
+               blend_radius=0.099)
+    seq.append(Gripper(goal=GRIPPER_POSE_CLOSED))
+    seq.append(Ptp(goal=Pose(position=Point(0, 0.2, .9), orientation=Quaternion(0,0,0,1)),
                    vel_scale=SLOW_VEL_SCALE,
                    acc_scale=ACC_SCALE))
 
+    seq.append(Gripper(goal=GRIPPER_POSE_OPEN))
     seq.append(Gripper(goal=GRIPPER_POSE_CLOSED))
 
     print("start sequence 2")
     r.move(seq)
     print("end sequence 2")
+
+    seq = Sequence()
+    seq.append(Gripper(goal=GRIPPER_POSE_CLOSED))
+
+    seq.append(Ptp(goal=Pose(position=Point(0, 0.2, .9), orientation=Quaternion(0,0,0,1)),
+        vel_scale=SLOW_VEL_SCALE,
+        acc_scale=ACC_SCALE))
+    seq.append(Ptp(goal=Pose(position=Point(0.2, 0.2, .9), orientation=Quaternion(0,0,0,1)),
+                   vel_scale=SLOW_VEL_SCALE,
+                   acc_scale=ACC_SCALE),
+               blend_radius=0.19)
+    seq.append(Gripper(goal=GRIPPER_POSE_OPEN))
+    seq.append(Ptp(goal=Pose(position=Point(0.2, 0, .9), orientation=Quaternion(0,0,0,1)),
+                   vel_scale=SLOW_VEL_SCALE,
+                   acc_scale=ACC_SCALE))
+
+    seq.append(Gripper(goal=GRIPPER_POSE_CLOSED))
+
+    print("start sequence 3")
+    r.move(seq)
+    print("end sequence 3")
 
 if __name__ == "__main__":
     # init a rosnode
