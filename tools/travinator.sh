40400c6469918f56d384580d41f61b2cca3b49c9
Christian Henkel <post@henkelchristian.de>
2019-08-13 17:17:05 +0200

    checking out the branch, first

--- tools/travinator.sh
+++ tools/travinator.sh
@@ -2,10 +2,10 @@
 # this tool takes a number and a branch name as arguments e.g.: travinator.sh 10 fix/my_branch
 # It will trigger 10 travis builds in the current repo by pushing empty commits to this branch
 # to use this, you need to install the travis cli. (https://rubygems.org/gems/travis)
-#  > sudo apt install ruby ruby-dev gem 
+#  > sudo apt install ruby ruby-dev gem
 #  > gem install travis
-# Note: check before whether the `travis status` command is working. 
-# You will have to login first and you may need to set the repo 
+# Note: check before whether the `travis status` command is working.
+# You will have to login first and you may need to set the repo
 # with `--store-repo` (https://www.rubydoc.info/gems/travis/1.8.10#status)
 
 N=$1
@@ -36,6 +36,11 @@ for ((i = 1 ; i <= $N ; i++)); do
     sleep 1s
   done
   echo "build.."
+  git checkout $BRANCH
+  if [ "$?" -ne "0" ]; then
+    echo "Could not check out $BRANCH"
+    exit ${1}
+  fi
   git commit --allow-empty -m"trigger travis $i"
   git push origin $BRANCH
   sleep 5s
